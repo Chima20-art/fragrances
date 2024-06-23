7a8e67c5269010454d8492b099cc889c3e14460f
taoufiq lotfi <leh.lotfi@gmail.com>
2024-06-23 15:32:49 +0100

    refactor: Improve product grid layout and add "No products found" message
    This commit refactors the product grid layout in the search page to improve the visual presentation. It also adds a "No products found" message to be displayed when there are no products available. These changes enhance the user experience by providing a more visually appealing layout and clear messaging when no products are found.

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -107,36 +107,42 @@ export default async function Search({
               <span className="absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-primary h-[3px] w-20"></span>
             </div>
 
-            <div className="products grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 lg:gap-4 gap-3">
-              {products.map((product: any) => (
-                <Link key={product._id} href={`/produits/${product._id}`}>
-                  <div className="border border-gray-300 rounded-md flex flex-col min-h-[350px] cursor-pointer h-full">
-                    <div className="image pb-3 h-3/4 bg-secondary">
-                      <img
-                        src={urlForImage(product.image)}
-                        alt={product.title}
-                        className="w-full h-full object-contain"
-                      />
-                    </div>
-                    <div className="h-1/4 flex items-center flex-col px-3 py-2">
-                      <h2 className="text-primary font-bold text-center">
-                        {product.title}
-                      </h2>
-                      <div className="flex justify-center">
-                        {product.oldprice && (
-                          <span className="old-price text-sm line-through text-gray-400 mr-2">
-                            {product.oldprice}dh
+            {products.length > 0 ? (
+              <div className="products grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 lg:gap-4 gap-3">
+                {products.map((product: any) => (
+                  <Link key={product._id} href={`/produits/${product._id}`}>
+                    <div className="border border-gray-300 rounded-md flex flex-col min-h-[350px] cursor-pointer h-full">
+                      <div className="image pb-3 h-3/4 bg-secondary">
+                        <img
+                          src={urlForImage(product.image)}
+                          alt={product.title}
+                          className="w-full h-full object-contain"
+                        />
+                      </div>
+                      <div className="h-1/4 flex items-center flex-col px-3 py-2">
+                        <h2 className="text-primary font-bold text-center">
+                          {product.title}
+                        </h2>
+                        <div className="flex justify-center">
+                          {product.oldprice && (
+                            <span className="old-price text-sm line-through text-gray-400 mr-2">
+                              {product.oldprice}dh
+                            </span>
+                          )}
+                          <span className="current-price text-sm font-bold">
+                            {product.price}dh
                           </span>
-                        )}
-                        <span className="current-price text-sm font-bold">
-                          {product.price}dh
-                        </span>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </Link>
-              ))}
-            </div>
+                  </Link>
+                ))}
+              </div>
+            ) : (
+              <div className="text-center text-2xl font-bold text-primary">
+                No products found
+              </div>
+            )}
           </div>
         </div>
       </div>
